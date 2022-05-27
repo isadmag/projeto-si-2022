9b4b997fd3e3c5de85cbba0d4a4556f68243c5f2
Isabela <isabelamagnani@hotmail.com>
2022-05-26 22:00:07 -0300

    Iniciando o versionamento

---
+++ ProjetoBanco/Classes/ContaCorrente.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoBanco.Classes
+{
+    public class ContaCorrente : Conta
+    {
+        public ContaCorrente(Cliente cliente, int numero, decimal saldo) : base(cliente, numero, saldo)
+        { }
+      
+        public override bool Depositar(decimal valorDeposito)
+        {
+            if (valorDeposito <= 0)
+            {
+                MensagemTransacoes = $"Não é possível realizar o depósito!";
+                return false;
+            }
+
+            Saldo += valorDeposito;
+            MensagemTransacoes = "Depósito realizado com sucesso!";
+
+            return true;
+        }
+
+        public override bool Sacar(decimal valorSaque)
+        {
+            if (Saldo <= 0m)
+            {
+                MensagemTransacoes = $"Saldo insuficiente para saque. Sua conta está com o valor atual de {Saldo}";
+                return false;
+            }
+
+            if (Saldo < valorSaque)
+            {
+                MensagemTransacoes = $"Valor solicitado para o saque é {valorSaque} e o Saldo é {Saldo}";
+                return false;
+            }
+
+            Saldo -= valorSaque;
+            return true;
+        }
+
+       
+    }
+}
+
---
+++ ProjetoBanco/Program.cs
@@ -0,0 +1,44 @@
+﻿using ProjetoBanco.Classes;
+using System;
+
+namespace ProjetoBanco
+{
+    class Program
+    {
+        static void Main()
+        {            
+            try
+            {
+                var mauricio = Cliente.CreateCliente("Maurício", "Rua 9 de Julho 202", 16999222999, "999.999.999-99", new DateTime(2000, 02, 17));
+                var roger = Cliente.CreateCliente("Roger", "Rua São Bento 304", 16999222998, "251.851.986-25", new DateTime(2000, 02, 17));
+                
+                Console.WriteLine(mauricio.ToString());
+
+                
+                Conta contaPoupanca = new Poupanca(mauricio, 445852, 1000);
+                Conta contaCorrente = new ContaCorrente(roger, 999999, 0);
+
+                Console.WriteLine(contaPoupanca.Saldo);
+
+                contaPoupanca.Transferir(500, contaCorrente);
+
+                Console.WriteLine(contaPoupanca.Saldo);
+                contaPoupanca.Depositar(1000);
+                Console.WriteLine(contaPoupanca.Saldo);
+                contaPoupanca.Sacar(500);
+                Console.WriteLine(contaPoupanca.Saldo);
+
+                Console.WriteLine(contaCorrente.Saldo);
+                contaCorrente.Depositar(5500);
+                Console.WriteLine(contaCorrente.Saldo);
+                contaCorrente.Sacar(500);
+                Console.WriteLine(contaCorrente.Saldo);
+
+            }            
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            } 
+        }
+    }
+}
